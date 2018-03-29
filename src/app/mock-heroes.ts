1cd4c3bc9c9ff6e304477827917d1441127fa36d
Geda <g.murinaite@gmail.com>
2018-03-29 18:39:26 +0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Harry' },
+    { id: 12, name: 'Hermione' },
+    { id: 13, name: 'Dumbledore' },
+    { id: 14, name: 'Granger' },
+    { id: 15, name: 'He Who Must Not Be Named' },
+    { id: 16, name: 'Dobby' },
+    { id: 17, name: 'Ron' },
+    { id: 18, name: 'Severus' },
+    { id: 19, name: 'DobbyIsAFreeElf' },
+    { id: 20, name: 'Avada Kedavra' },
+]
